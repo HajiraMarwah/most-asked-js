62bc75ccd83da3b1cde3d793675c7c57e2b652ce
Hajira Marwah <hajiranadia12@gmail.com>
2025-10-31 23:30:13 +0530

    merge two sorted array

---
+++ arrays/mergeTwoSorted.js
@@ -0,0 +1,31 @@
+function mergeSortedArrays(arr1, arr2) {
+  let i = 0, j = 0;
+  let merged = [];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+
+  // Add remaining elements from arr1
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
+
+  // Add remaining elements from arr2
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+
+  return merged;
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6]));
+// Output: [1, 2, 3, 4, 5, 6]
