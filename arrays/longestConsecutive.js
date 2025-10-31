2b98620237b44d9ced80dd7130305ba2ec596e2c
Hajira Marwah <hajiranadia12@gmail.com>
2025-10-31 23:35:43 +0530

    longest consecutive sequence

---
+++ arrays/longestConsecutive.js
@@ -0,0 +1,24 @@
+function longestConsecutive(nums) {
+  let numSet = new Set(nums);
+  let longest = 0;
+
+  for (let num of numSet) {
+    // Only start counting if num-1 is not present
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+
+      longest = Math.max(longest, currentStreak);
+    }
+  }
+
+  return longest;
+}
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
+// Output: 4
